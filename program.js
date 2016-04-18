bbac7c3da1865955045c669613c9fac8944da8b2
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-18 17:53:58 +0200

    4

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-process.stdin.pipe(process.stdout);
+var transform = require('through2')(function (buffer, _, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}, function (done) {
+  done();
+});
+
+process.stdin.pipe(transform).pipe(process.stdout);
