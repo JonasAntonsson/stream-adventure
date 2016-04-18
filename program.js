327c20d64a8d7ab36aeb1b7333e453811b7fd540
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-18 17:41:46 +0200

    2

--- program.js
+++ program.js
@@ -1,1 +1,12 @@
-console.log("beep boop");
+var fs = require('fs');
+
+function run(filepath) {
+  fs.createReadStream(filepath).pipe(process.stdout);
+}
+
+if( process.argv.length > 2 ) {
+  run(process.argv[2]);
+} else {
+  console.log("Usage: node program.js <filepath>");
+}
+
