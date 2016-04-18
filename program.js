901adac3572b03810e9c17f5c98a0b3f0493071d
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-18 18:04:15 +0200

    5

--- program.js
+++ program.js
@@ -1,8 +1,12 @@
-var transform = require('through2')(function (buffer, _, next) {
-  this.push(buffer.toString().toUpperCase());
-  next();
-}, function (done) {
-  done();
-});
+var split = require("split");
+var through2 = require("through2");
+var even = false;
 
-process.stdin.pipe(transform).pipe(process.stdout);
+process.stdin.pipe(split()).pipe(through2(function (buffer, _, next) {
+  if( even )
+    this.push(buffer.toString().toUpperCase() + "\n");
+  else
+    this.push(buffer.toString().toLowerCase() + "\n");
+  even = !even;
+  next();
+})).pipe(process.stdout);
