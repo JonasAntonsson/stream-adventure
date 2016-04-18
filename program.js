fb79149055ff7e25950759ce7fa318bff460d888
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-18 17:45:02 +0200

    3

--- program.js
+++ program.js
@@ -1,12 +1,1 @@
-var fs = require('fs');
-
-function run(filepath) {
-  fs.createReadStream(filepath).pipe(process.stdout);
-}
-
-if( process.argv.length > 2 ) {
-  run(process.argv[2]);
-} else {
-  console.log("Usage: node program.js <filepath>");
-}
-
+process.stdin.pipe(process.stdout);
