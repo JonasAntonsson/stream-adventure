38dce15b50997de6b7921f981801d5452d819bd1
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-19 10:35:08 +0200

    11

--- program.js
+++ program.js
@@ -1,14 +1,7 @@
-var through = require("through2");
-var trumpet = require("trumpet")();
+var spawn = require("child_process").spawn;
+var duplex = require("duplexer2");
 
-var toUpper = through(function (buf, _, next) {
-  this.push(buf.toString().toUpperCase());
-  next();
-});
-
-
-var selectedStream = process.stdin.pipe(trumpet).select(".loud").createStream();
-
-selectedStream.pipe(toUpper).pipe(selectedStream);
-
-trumpet.pipe(process.stdout);
+module.exports = function (cmd, args) {
+  var child = spawn(cmd, args);
+  return duplex(child.stdin, child.stdout);
+}
