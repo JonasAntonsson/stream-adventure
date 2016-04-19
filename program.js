72a018439e0c0cd844bfe5a1ab34f317a3b45fc0
Jonas Antonsson <jonas_antonsson@yahoo.se>
2016-04-19 10:16:36 +0200

    10

--- program.js
+++ program.js
@@ -1,2 +1,14 @@
-var ws = require("websocket-stream");
-ws("ws://localhost:8099").write("hello\n");
+var through = require("through2");
+var trumpet = require("trumpet")();
+
+var toUpper = through(function (buf, _, next) {
+  this.push(buf.toString().toUpperCase());
+  next();
+});
+
+
+var selectedStream = process.stdin.pipe(trumpet).select(".loud").createStream();
+
+selectedStream.pipe(toUpper).pipe(selectedStream);
+
+trumpet.pipe(process.stdout);
